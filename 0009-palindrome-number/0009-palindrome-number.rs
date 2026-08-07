576e1c0023e88f6f1bcbbe29025c3b38cf199517
Vivek Jha <vjha2216@gmail.com>
2026-08-07 22:12:27 +0530

    Time: 3 ms (42.04%), Space: 2.1 MB (52.8%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 {
+            return false;
+        }
+
+        let original = x;
+        let mut num = x;
+        let mut rev = 0;
+
+        while num > 0 {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+
+        original == rev
+    }
+}
