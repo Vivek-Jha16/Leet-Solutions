ebfac290073c2762a1a000eb26b9fd7adbf6ba4d
Vivek Jha <vjha2216@gmail.com>
2026-08-08 22:37:02 +0530

    Time: 1 ms (37.36%), Space: 14.7 MB (85.38%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.c
@@ -0,0 +1,21 @@
+int maxArea(int* height, int heightSize) {
+    int left = 0;
+    int right = heightSize - 1;
+    int maxArea = 0;
+
+    while (left < right) {
+        int width = right - left;
+        int h = height[left] < height[right] ? height[left] : height[right];
+        int area = width * h;
+
+        if (area > maxArea)
+            maxArea = area;
+
+        if (height[left] < height[right])
+            left++;
+        else
+            right--;
+    }
+
+    return maxArea;
+}
